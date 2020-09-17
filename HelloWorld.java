eca83123bbf6b2ae491bda9643567bb5b62a320e
lavtirinku <lavtirinku@gmail.com>
2020-09-17 15:40:35 +0000

    Added second print line

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 //This Java program will print message on the commans line
 public class HelloWorld {
 	  public static void main(String[] args){
+		  System.out.println("second line");
 		      System.out.println("This is my first program in java");
 		        }//End of main
 }//End of FirstJavaProgram Clas
